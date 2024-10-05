4b22c237a2e92c1bdd28ae8f834584eefa0d44e3
Vitokhin Eldar <hinc2iovem@gmail.com>
2024-10-05 12:14:29 +0500

    fixing amplify routes problem

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -5,8 +5,10 @@ const BASE_URL =
     ? import.meta.env.VITE_PROD_PORT
     : "http://localhost:3500";
 
+console.log(BASE_URL);
+
 export const axiosCustomized = axios.create({
-  baseURL: BASE_URL,
+  baseURL: `${BASE_URL}/api`,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
