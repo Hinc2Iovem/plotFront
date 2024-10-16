227fcad679d75166ff23fbf4170b45503584b1b4
Vitokhin Eldar <hinc2iovem@gmail.com>
2024-10-17 01:14:39 +0500

    forgot to turn on dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ import { useEffect } from "react";
 
 export default function App() {
   useEffect(() => {
-    localStorage.setItem("theme", "light");
+    localStorage.setItem("theme", "dark");
 
     const selectedTheme = localStorage.getItem("theme");
 
