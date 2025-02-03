00d134ba1f54758eaa7c8f4281b3efd19b577008
Vitokhin Eldar <hinc2iovem@gmail.com>
2025-02-04 02:09:44 +0500

    url test 1

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL =
   import.meta.env.VITE_NODE_ENV === "production"
-    ? import.meta.env.VITE_PROD_PORT
+    ? `https://${import.meta.env.VITE_PROD_PORT}`
     : "http://localhost:3000";
 
 console.log(BASE_URL);
